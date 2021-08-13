83220ebb1f91f7ba88979a24bfd6a61c36ff948a
LifepayDickson <dickson.lim@lifepay.com.au>
2021-08-13 10:26:37 +1000

    writing jest test to make sure json result is returning as expected

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders if main App is successfully loaded', () => {
   render(<App />);
   const linkElement = screen.getByText(/Medibank - Display Owners Pet By Gender/i);
   expect(linkElement).toBeInTheDocument();
