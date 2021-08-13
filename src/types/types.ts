7263a2d1c8cc1031adee5f0a922a3374a0a30ae8
LifepayDickson <dickson.lim@lifepay.com.au>
2021-08-13 13:11:12 +1000

    try to break user pets data onto single user with pets array

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,3 +16,7 @@ interface PetsName {
   name: string;
   type: string;
 }
+
+export interface Concat<T> {
+  concat<T1>(...items: T1[]): (T | (T1 extends (infer T1)[] ? T1 : T1))[];
+}
--- src/utils/SortData.ts
+++ src/utils/SortData.ts
@@ -1,23 +1,29 @@
-import { Pets, Owner } from "../types";
+import { Pets, Owner, Concat } from "../types";
 
-export const sortUserPetData = (data: Owner) => {
+// Reverse engineering to break user multiple pets to single user with their respective pets.
+export const sortOwnerPetData = (data: Owner) => {
+  // Do a validation check when reading data, check if Array data is empty
   if (data === undefined || !Array.isArray(data) || data.length === 0) {
     return [];
   }
-  let cats = data.reduce((collection, owner: Owner) => {
-    console.log("+++++++++++++++=", collection);
+
+  // Executes a reducer function on each element of the array, resulting in a single output value.
+  let ownerPet = data.reduce((collection: Concat<Pets>, owner: Owner) => {
+    // Check if this owner has pets
     if (owner.pets) {
+      // Creates a new array populated with the results of calling a provided function on every element in the calling array.
       let pets = owner.pets.map((pet: Pets) => {
-        let userMergedPet: Pets = {};
-        Object.assign(userMergedPet, pet);
-        userMergedPet.userName = owner.name;
-        userMergedPet.userGender = owner.gender;
-        return userMergedPet;
+        let mergePetOwner: Pets = {};
+        // Copies all enumerable owner properties from one or more source objects to a target object. It returns the modified target object.
+        Object.assign(mergePetOwner, pet);
+        mergePetOwner.userName = owner.name;
+        mergePetOwner.userGender = owner.gender;
+        return mergePetOwner;
       });
       return collection.concat(pets);
     } else {
       return collection;
     }
   }, []);
-  return cats;
+  return ownerPet;
 };
