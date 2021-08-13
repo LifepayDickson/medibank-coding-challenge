d6606dc2e032c5fd456363c3be4f07105673fc29
LifepayDickson <dickson.lim@lifepay.com.au>
2021-08-13 11:01:35 +1000

    checking if webservice data is returning the overall results

---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export interface Pets {
+  userName?: string;
+  userGender?: string;
+  name?: String;
+  type?: string;
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,19 @@
 import * as React from "react";
+import { getDataResults } from "../api";
+import { Pets } from "../types";
 
 const Home: React.FC = () => {
+  const [userData, setUserData] = React.useState<Pets>();
+
+  React.useEffect(() => {
+    // Should Start grabbing data from webServices"
+    const load = async () => {
+      setUserData(await getDataResults());
+    };
+    load();
+  }, []);
+
+  console.log("========", userData);
 
   return (
     <>
